43f76b85722fa2e9270b894f1e7066dbc27cb339
Illia Lukianov <illia.lukianov@wallchain.xyz>
2024-03-26 09:40:54 +0800

    feat: add test version func

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { createAAAccountClientFromPrivateKeyAccount } from "./SavingsSDK";
-export { getSupportedDepositStrategies } from "./depositStrategies";
-export { createSavingsAccount } from "./SavingsAccount/createSavingsAccount";
-export { SavingsAccount } from "./SavingsAccount/SavingsAccount";
+export { createAAAccountClientFromPrivateKeyAccount } from './SavingsSDK';
+export { getSupportedDepositStrategies } from './depositStrategies';
+export { createSavingsAccount } from './SavingsAccount/createSavingsAccount';
+export { SavingsAccount } from './SavingsAccount/SavingsAccount';
+
+export function version() {
+  return '1.0.0-alpha.2';
+}
