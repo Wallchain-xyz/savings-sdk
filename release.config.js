4c85983a83534fdbffc0f5b863c040d30db6dff5
Illia Lukianov <illia.lukianov@wallchain.xyz>
2024-04-03 12:33:48 +0800

    feat(release): try to add changelog generation

--- release.config.js
+++ release.config.js
@@ -3,13 +3,18 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
     '@semantic-release/gitlab',
     '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['package.json', 'docs/CHANGELOG.md'],
         message:
           // eslint-disable-next-line no-template-curly-in-string
           'chore(release): ${nextRelease.version} [skip ci]',
