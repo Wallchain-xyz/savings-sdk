ba1b1277b7c9a45dc6049c076e1c49246528fbb3
Illia Lukianov <illia.lukianov@wallchain.xyz>
2024-04-11 15:23:47 +0800

    feat: eng-1980: replace js with json strategies

--- src/depositStrategies/getSupportedStrategies.ts
+++ src/depositStrategies/getSupportedStrategies.ts
@@ -1,12 +1,21 @@
-import { beefyBaseNativeStrategy } from './strategies/beefy/beefyBaseNativeStrategy';
-// import { beefyBscNativeStrategy } from './strategies/beefy/beefyBscNativeStrategy';
+import beefyBaseNativeStrategies from './strategies/base/deposit_strategies.json';
+// commented out for now, since gas on BNB is higher than on BASE
+// import beefyBscNativeStrategies from './strategies/bsc/deposit_strategies.json';
 
 import type { DepositStrategy } from './DepositStrategy';
 
+const prepareBeefyStrategy = (strategy: (typeof beefyBaseNativeStrategies)[number]) =>
+  ({
+    ...strategy,
+    permissions: strategy.permissions.map(permission => ({
+      ...permission,
+      valueLimit: BigInt(permission.valueLimit),
+    })),
+  } as DepositStrategy);
+
 export function getSupportedDepositStrategies(): DepositStrategy[] {
   return [
-    // commented out for now, since gas on BNB is higher than on BASE
-    // beefyBscNativeStrategy,
-    beefyBaseNativeStrategy,
-  ];
+    ...beefyBaseNativeStrategies,
+    // ...beefyBscNativeStrategies
+  ].map(prepareBeefyStrategy);
 }
