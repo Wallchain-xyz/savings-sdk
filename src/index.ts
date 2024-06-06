4acae7092699b2f5a02385307a691a3cc97b04e0
Roman Skurikhin <romasku135@gmail.com>
2024-06-06 13:57:43 +0300

    chore: export DepositStrategy

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { createSKAccount } from './factories/createSKAccount';
 export { createSavingsAccountFromPrivateKeyAccount } from './factories/createSavingsAccountFromPrivateKeyAccount';
 export { createStrategiesManager } from './factories/createStrategiesManager';
 export { SavingsAccount } from './SavingsAccount/SavingsAccount';
-export { DepositStrategyType } from './depositStrategies/DepositStrategy';
+export type { DepositStrategy, DepositStrategyType } from './depositStrategies/DepositStrategy';
 export type { SavingsAccountUserId } from './api/SavingsBackendClient';
 export type { User as SavingsAccountUser } from './api/auth/__generated__/createApiClient';
 // TODO: @merlin we need to make this universal and part of our code
