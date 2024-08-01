7ce1a7d7874c5c2bb42d83bdf86e25daa0de1a14
Roman Skurikhin <romasku135@gmail.com>
2024-08-01 13:07:41 +0300

    feat: add info about points for mellow

--- src/depositStrategies/pointsByStrategyId.ts
+++ src/depositStrategies/pointsByStrategyId.ts
@@ -23,8 +23,31 @@ const EtherfiKingKarakLrtPoints: APIPoint[] = [
   },
 ];
 
+const MellowRenzoLrtPoints: APIPoint[] = [
+  {
+    // TODO:@merlin images should be ours and served from backend
+    imageUrl: 'https://avatars.githubusercontent.com/u/83597631?s=200',
+    name: 'Mellow',
+    multiplier: 1,
+  },
+  {
+    // TODO:@merlin images should be ours and served from backend
+    imageUrl: 'https://s2.coinmarketcap.com/static/img/coins/200x200/32192.png',
+    name: 'Symbiotic',
+    multiplier: 1,
+  },
+  {
+    // TODO:@merlin images should be ours and served from backend
+    imageUrl: 'https://s2.coinmarketcap.com/static/img/coins/200x200/30843.png',
+    name: 'Renzo',
+    multiplier: 1.5,
+  },
+];
+
 // TODO:@merlin temp solution, should be in DS
 export const pointsByStrategyId: Partial<Record<StrategyId, APIPoint[]>> = {
   'c38d9a08-a0de-4866-bf16-e433a03848ff': EtherfiKingKarakLrtPoints,
   'c1d136de-ee0c-4652-9708-836939241d3a': EtherfiKingKarakLrtPoints,
+  '5155d89f-98c3-436b-bd87-d8fef022620a': MellowRenzoLrtPoints,
+  'a4be0324-c93c-4525-a0d1-48c6f9f1bb49': MellowRenzoLrtPoints,
 };
